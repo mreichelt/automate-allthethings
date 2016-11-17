6fbf807e3a5a2b0d0565eac4ff3e117c23030995
Marc Reichelt <mcreichelt@gmail.com>
2016-11-17 15:35:15 +0100

    save APK size to plot on Jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 ./gradlew clean app:assembleDebug testDebug
+
+OUTFILE=app/build/outputs/apksize.csv
+echo filesize > $OUTFILE
+stat -f%z app/build/outputs/apk/app-debug.apk >> $OUTFILE
